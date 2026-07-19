b5479c60c4f0ba58794330f0350e3fce4e08d17f
abhinay2409 <abhinayg100@gmail.com>
2026-07-20 01:50:26 +0530

    Created GetCategoriesUseCase in domain usecase
    Signed-off-by: abhinay2409 <abhinayg100@gmail.com>

---
+++ composeApp/src/commonMain/kotlin/com/adrinova/productbrowser/domain/usecase/GetCategoriesUseCase.kt
@@ -0,0 +1,12 @@
+package com.adrinova.productbrowser.domain.usecase
+
+import com.adrinova.productbrowser.domain.model.Category
+import com.adrinova.productbrowser.domain.repository.ProductRepository
+
+/**
+ * Created by Abhinay on 20/07/26.
+ */
+/** Business use case: list all product categories (used for filtering). */
+private class GetCategoriesUseCase(private val repository: ProductRepository) {
+    suspend operator fun invoke(): Result<List<Category>> = repository.getCategories()
+}
