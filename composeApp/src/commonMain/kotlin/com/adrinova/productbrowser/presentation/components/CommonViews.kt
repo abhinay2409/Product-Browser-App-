86c06be40a67d0fdc07c36e732804958127f6fab
abhinay2409 <abhinayg100@gmail.com>
2026-07-25 23:57:30 +0530

    Added CircularProgressIndicator() in presentation layer
    Signed-off-by: abhinay2409 <abhinayg100@gmail.com>

---
+++ composeApp/src/commonMain/kotlin/com/adrinova/productbrowser/presentation/components/CommonViews.kt
@@ -0,0 +1,24 @@
+package com.adrinova.productbrowser.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+/**
+ * Created by Abhinay on 25/07/26.
+ */
+@Composable
+fun LoadingView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+
+}
